ad71e90b14a5f842113e37f08b3c17c56abce124
unknown <isemzr@gmail.com>
2022-06-21 11:19:56 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './COMPOENT/Header';
+import Person from './COMPOENT/Person';
+import axios from 'axios';
+import { useState} from 'react';
+import { useEffect } from 'react';
+
 
 function App() {
+const [Persons, setPerson] = useState([]);
+useEffect(() => {
+  try {
+    let result =  axios.get("https://jsonplaceholder.typicode.com/users").then((res)=> setPerson(res.data));
+  } catch (error) {
+    console.log(error)
+  }
+
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+<Header/>
+<div className="list ">
+{Persons.length?Persons.map(el => <Person user = {el}/>):<img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921" alt="" /> }
+
+</div>
+
     </div>
   );
 }
